e250cde5dff6acc25fd76da47a6f201703e05c4b
John Pedrotti <jdpedrotti@gmail.com>
2022-11-22 12:09:37 -0600

    initialized repo

---
+++ src/main/java/com/codeup/springblogpostproject/SpringBlogpostProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springblogpostproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogpostProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogpostProjectApplication.class, args);
+    }
+
+}
