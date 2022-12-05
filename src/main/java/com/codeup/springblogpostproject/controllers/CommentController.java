0f3338057ff5354acf2435f0aeb3005da1240ed0
John Pedrotti <jdpedrotti@gmail.com>
2022-12-05 14:12:43 -0600

    pull from main

--- src/main/java/com/codeup/springblogpostproject/controllers/CommentController.java
+++ src/main/java/com/codeup/springblogpostproject/controllers/CommentController.java
@@ -9,6 +9,7 @@ import com.codeup.springblogpostproject.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,11 @@ public class CommentController {
         this.postsDao = postsDao;
     }
 
+    @GetMapping("/create")
+    public String createComment(Model model) {
+        model.addAttribute("comment", new Comment());
+        return "/comments/create";
+    }
 
     @PostMapping("/{id}/create")
     public String addComment(@ModelAttribute Comment comment) {
