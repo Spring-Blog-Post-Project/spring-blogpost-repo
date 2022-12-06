95a02467e732717ddcb34be06a7fb02f01486295
Stephen Pflug <stephen@pflug.us>
2022-12-05 19:49:37 -0600

    added comments to controllers and securityconfiguration

--- src/main/java/com/codeup/springblogpostproject/controllers/HomeController.java
+++ src/main/java/com/codeup/springblogpostproject/controllers/HomeController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // Shows splash_page.html template
     @GetMapping("/")
     public String helloWorld() {
         return "splash_page";
     }
 
+    // Shows about.html template
     @GetMapping("/about")
     public String aboutUs(){
         return "about";
