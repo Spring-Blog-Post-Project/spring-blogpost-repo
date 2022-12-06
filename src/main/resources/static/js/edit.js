9a4147b43085c6ed35df7a0eb2d1d8932f964abf
Stephen Pflug <stephen@pflug.us>
2022-12-05 19:56:54 -0600

    made spacing similar on all css stylesheets, added comments to js files

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Event listener to redirect when .editButton clicked
     $(".editButton").on('click', function(e){
         window.location.replace(`/posts/${$(this).attr("data-id")}/edit`);
     });
